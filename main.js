a3182b567f6051b5f601813e74bb2462184590f9
StephaneC <Stephane.castrec@gmail.com>
2017-10-19 20:56:58 +0200

    allow CORS

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ connectRedis();
 
 app.use(express.static('./html/docapi'));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 
 app.post('/ping', function(req, res){
     res.setHeader('Content-Type', 'text/json');
